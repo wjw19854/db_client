67a99d8895f4caf7eb2e56ce03c0df0e03ba7fc2
王嘉炜 <jiawei.wang@ikang.com>
2018-04-27 17:18:23 +0800

    增加SQL高亮并增加退出功能

--- src/db_client.py
+++ src/db_client.py
@@ -1,15 +1,18 @@
-# coding=utf-8
-
+# coding=utf8
 from __future__ import unicode_literals
 from prompt_toolkit import prompt
 from prompt_toolkit.history import InMemoryHistory
+from pygments.lexers import SqlLexer
 
 def main():
     history = InMemoryHistory()
 
     while True:
-        text = prompt("> ", history=history)
+        text = prompt('> ', lexer=SqlLexer, history=history)
         print('You entered:', text)
+        if text.lower() == 'exit':
+            # 退出
+            break
     print('GoodBye!')
 
 if __name__ == '__main__':
