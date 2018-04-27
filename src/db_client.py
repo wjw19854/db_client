6ff46f0d81b6a77313ecf7496333fa6b377ce79f
王嘉炜 <jiawei.wang@ikang.com>
2018-04-27 17:09:52 +0800

    增加循环输入

--- src/db_client.py
+++ src/db_client.py
@@ -2,10 +2,15 @@
 
 from __future__ import unicode_literals
 from prompt_toolkit import prompt
+from prompt_toolkit.history import InMemoryHistory
 
 def main():
-    text = prompt("> ")
-    print('You entered:', text)
+    history = InMemoryHistory()
+
+    while True:
+        text = prompt("> ", history=history)
+        print('You entered:', text)
+    print('GoodBye!')
 
 if __name__ == '__main__':
     main()
